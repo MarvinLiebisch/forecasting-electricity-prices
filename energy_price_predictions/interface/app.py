e2cf518ec52efb017f5a630df8b97d2e0d8419a7
Marvin Liebisch <marvin.liebisch@gmail.com>
2023-03-27 17:07:41 +0700

    Some plot ideas

--- energy_price_predictions/interface/app.py
+++ energy_price_predictions/interface/app.py
@@ -23,18 +23,34 @@ sys.path.insert(0,git_path)
 
 from energy_price_predictions.ml_logic.data_import import import_merged_data
 
+st.set_page_config(layout="wide")
 
 @st.cache
 def load_data():
     df = import_merged_data()
     df_price = df[['price_day_ahead']]
+    df_price['hour'] = df_price.index.hour+1
     df_price['7D_SMA'] = df_price['price_day_ahead'].rolling(24*7).mean()
     df_price['30D_SMA'] = df_price['price_day_ahead'].rolling(24*30).mean()
     df_price.dropna(inplace=True)
-    return df_price
+
+    dict_of_hourly_df = {}
+    for i in range(24):
+        key_name = i+1
+        df_new = df_price[['price_day_ahead']][df_price['hour'] == i+1]
+        df_new['30D_SMA'] = df_new['price_day_ahead'].rolling(30).mean()
+        df_new['14D_SMA'] = df_new['price_day_ahead'].rolling(14).mean()
+        df_new['7D_SMA'] = df_new['price_day_ahead'].rolling(7).mean()
+        df_new['3D_SMA'] = df_new['price_day_ahead'].rolling(3).mean()
+        df_new.dropna(inplace=True)
+        dict_of_hourly_df[key_name] = df_new
+
+    return df_price, dict_of_hourly_df
 
 
-df_price = load_data()
+df_price, dict_of_hourly_df = load_data()
+
+
 
 '''
 # Day-Ahead Energy Price Prediction Model
@@ -52,7 +68,7 @@ def plot_1(df_price):
                                                                                                         'rgba(255, 255, 255, 1)'])
 
 
-    fig_copy = fig.update_layout(legend=dict(
+    fig = fig.update_layout(legend=dict(
         yanchor="top",
         y=0.99,
         xanchor="left",
@@ -60,15 +76,14 @@ def plot_1(df_price):
     ))
 
     newnames = {'30D_SMA':'30D SMA', 'price_day_ahead': 'Hourly Price'}
-    fig_copy.for_each_trace(lambda t: t.update(name = newnames[t.name],
+    fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
                                         legendgroup = newnames[t.name],
                                         hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
                                         )
                     )
 
-    fig_copy.update_layout(
+    fig.update_layout(
         title="Average Day-ahead Auction Prices",
-        xaxis_title="",
         yaxis_title="Price",
         legend_title=None,
         font=dict(
@@ -77,26 +92,209 @@ def plot_1(df_price):
             color="white"
         )
     )
-    return fig_copy
 
-fig1 = plot_1(df_price)
-st.plotly_chart(fig1)
+    xaxis=dict(
+        title=None,
+        showgrid=False,
+        showline=False,
+        zeroline=False,
+
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=3, label="3m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+
+    fig.update_layout(
+    xaxis=xaxis,
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    template='plotly_dark',
+    xaxis_rangeselector_font_color='black',
+    xaxis_rangeselector_activecolor='gray',
+    xaxis_rangeselector_bgcolor='silver'
+    )
+
+
+
+    return fig
+
+fig = plot_1(df_price)
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 '''
 ## Hourly Auction Data
 '''
 
-# Generate placeholder data
-date_range = pd.date_range("2022-01-01", "2022-12-31", freq="D")
-df = pd.DataFrame()
-for i in range(24):
-    df[f"Series {i+1}"] = pd.Series(np.random.normal(100, 10, len(date_range)), index=date_range)
+hour = st.selectbox(
+    'Select an Hour from 1 to 24 to display',
+    (dict_of_hourly_df.keys()))
 
-option = st.selectbox(
-    'Hour to display',
-    (df.columns))
 
-st.write('You selected:', option)
+
+df = dict_of_hourly_df[hour]
+
+#def plot2(df, hour):
+
+# Create a Figure object with Scatter traces for each series
+fig = go.Figure()
+
+for i in df.columns:
+    if df[i].name != 'price_day_ahead':
+        fig.add_trace(go.Scatter(x=df.index, y=df[i], name=df[i].name))
+fig.add_trace(go.Scatter(x=df.index, y=df['price_day_ahead'], name='Price Day-Ahead'))
+
+# Add a legend and specify the trace order
+fig.update_layout(
+    legend=dict(traceorder="reversed"),
+    title=f"Day Ahead Price of Hour {hour}"
+)
+
+# Create a drop-down menu to show/hide each trace
+updatemenu = go.layout.Updatemenu(
+    buttons=list([
+        dict(
+            args=[{"visible": [True, False, False, False, True]}],
+            label="30D_SMA",
+            method="update"
+        ),
+        dict(
+            args=[{"visible": [False, True, False, False, True]}],
+            label="14D_SMA",
+            method="update"
+        ),
+        dict(
+            args=[{"visible": [False, False, True, False, True]}],
+            label="7D_SMA",
+            method="update"
+        ),
+        dict(
+            args=[{"visible": [False, False, False, True, True]}],
+            label="3D_SMA",
+            method="update"
+        ),
+        dict(
+            args=[{"visible": [True, True, True, True, True]}],
+            label="All",
+            method="update"
+        )
+    ]),
+    direction="down",
+    showactive=True,
+    x=1.2,
+    y=1.15,
+    active = 5
+)
+
+fig.update_layout(
+    updatemenus=[updatemenu]
+)
+    #return fig
+
+
+xaxis=dict(
+title=None,
+showgrid=False,
+showline=False,
+zeroline=False,
+rangeselector=dict(
+    buttons=list([
+        dict(count=7, label="7d", step="day", stepmode="backward"),
+        dict(count=14, label="14d", step="day", stepmode="backward"),
+        dict(count=30, label="30d", step="day", stepmode="backward"),
+        dict(count=90, label="90d", step="day", stepmode="backward"),
+        dict(step="all"),
+        ])
+    )
+)
+
+fig.update_layout(
+xaxis=xaxis,
+paper_bgcolor='rgba(0,0,0,0)',
+plot_bgcolor='rgba(0,0,0,0)',
+template='plotly_dark',
+xaxis_rangeselector_font_color='black',
+xaxis_rangeselector_activecolor='gray',
+xaxis_rangeselector_bgcolor='silver'
+)
+
+#fig = plot2(dict_of_hourly_df[hour][-30:], hour)
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+
+
+# def plot_2(df, hour):
+
+#     xaxis=dict(
+#         title=None,
+#         showgrid=False,
+#         showline=False,
+#         zeroline=False,
+#         rangeselector=dict(
+#             buttons=list([
+#                 dict(count=3, label="3d", step="day", stepmode="backward"),
+#                 dict(count=7, label="7d", step="day", stepmode="backward"),
+#                 dict(count=14, label="14d", step="day", stepmode="backward"),
+#                 dict(count=30, label="30d", step="day", stepmode="backward"),
+#             ])
+#         )
+#     )
+#     yaxis=dict(
+#         title=f'Price per MWh',
+#         showgrid=True,
+#         showline=False,
+#         zeroline=False,
+#     )
+#     legend=dict(
+#         yanchor="bottom",
+#         y=-0.3,
+#         xanchor="left",
+#         x=0,
+#         title=None,
+#         orientation='h',
+#     )
+
+#     fig = px.line(df,
+#         x='time',
+#         y=['price_day_ahead', f"{}"],
+#         width=15,
+#     )
+
+#     fig.update_layout(
+#         xaxis=xaxis,
+#         yaxis=yaxis,
+#         legend=legend,
+#         paper_bgcolor='rgba(0,0,0,0)',
+#         plot_bgcolor='rgba(0,0,0,0)',
+#         template='plotly_dark',
+#         xaxis_rangeselector_font_color='black',
+#         xaxis_rangeselector_activecolor='gray',
+#         xaxis_rangeselector_bgcolor='silver',
+#     )
+
+#     # Update legend name
+#     newnames = {
+#         'price_day_ahead': 'Real price',
+#         'price_day_ahead_predicted': 'Predicted price',
+#     }
+#     fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
+#                                         legendgroup = newnames[t.name],
+#                                         hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name]),
+#                                         line = dict(width=1),
+#                                         )
+#                     )
+
+#     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+#     return fig
+
+# fig = plot_1(dict_of_hour_df[hour])
+# st.plotly_chart(fig)
 
 
 
